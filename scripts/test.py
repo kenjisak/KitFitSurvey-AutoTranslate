6a782c1c0fa3c0e1c31ee0e152aec221f10b915d
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-23 13:21:43 -0400

    tested first API translation request to DeepL

--- scripts/test.py
+++ scripts/test.py
@@ -1,12 +1,23 @@
-import pandas as pd
+import pandas
+import json
+import deepl
 
-# Load the Excel file into a DataFrame
-file_path = '../data/All Kit Fit Male Comments.xlsx'
-df = pd.read_excel(file_path, sheet_name='All Male Comments')
+# # Load the Excel file into a DataFrame
+# file_path = '../data/All Kit Fit Male Comments.xlsx'
+# df = pandas.read_excel(file_path, sheet_name='All Male Comments')
 
-# Iterate over each cell in the DataFrame
-for index, row in df.iterrows():
-    for col in df.columns:
-        # Check if the cell contains a text value (string)
-        if isinstance(row[col], str):
-            print(f"\nOriginal value in ({index}, {col}): {row[col]}")
+# # Iterate over each cell in the DataFrame
+# for index, row in df.iterrows():
+#     for col in df.columns:
+#         # Check if the cell contains a text value (string)
+#         if isinstance(row[col], str):
+#             print(f"\nOriginal value in ({index}, {col}): {row[col]}")
+
+auth_key = ""
+with open('../data/api_key.json', 'r') as file:
+    auth_key = json.load(file)['api-key']
+
+translator = deepl.Translator(auth_key)
+
+result = translator.translate_text("Je porte ma veste tous les jours et c'est très désagréable.", target_lang="EN-GB")
+print(result.text) 
