e08045ebb310e7ba9db93eb4c0feb79322da0851
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-23 14:28:51 -0400

    tested using autodetection of non primary language to translate

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 import pandas
 import json
 import deepl
+from autoDetectLang import detect_Language
 
 # # Load the Excel file into a DataFrame
 # file_path = '../data/All Kit Fit Male Comments.xlsx'
@@ -13,17 +14,28 @@ import deepl
 #         if isinstance(row[col], str):
 #             print(f"\nOriginal value in ({index}, {col}): {row[col]}")
 
+primary_language = "en" # used from spaCy docs for languages listed
+target_language = "EN-GB" # used from DeepL docs for languages listed
+
+# init DeepL Translator
 auth_key = ""
 with open('../data/api_key.json', 'r') as file:
     auth_key = json.load(file)['api-key']
 
 translator = deepl.Translator(auth_key)
 
-result = translator.translate_text("Je porte ma veste tous les jours et c'est très désagréable.", target_lang="EN-GB")
-print(result.text) 
-
 # TODO add spaCY to detect language before sending API request to help with rate limiting
 # might not need to implement exponential back off
 # make another excel sheet and cut french cells into new one
 # send one translation request uploading the whole excel file
 # then add back the translated version into
+
+def translate_to_english(sourceText):
+    if not detect_Language(sourceText) == primary_language: # if any other language than person spoken language then make DeepL API translation request
+
+        result = translator.translate_text(sourceText, target_lang = target_language)
+        
+        return result.text
+
+if __name__ == "__main__":
+    print(translate_to_english("Je porte ma veste tous les jours et c'est très désagréable."))
