20a9a736cc4280cec70b130de31f246c33dac8c9
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-23 15:03:52 -0400

    rough method to iterate through all cells and translate only necessary ones

--- scripts/test.py
+++ scripts/test.py
@@ -3,16 +3,11 @@ import json
 import deepl
 from autoDetectLang import detect_Language
 
-# # Load the Excel file into a DataFrame
-# file_path = '../data/All Kit Fit Male Comments.xlsx'
-# df = pandas.read_excel(file_path, sheet_name='All Male Comments')
-
-# # Iterate over each cell in the DataFrame
-# for index, row in df.iterrows():
-#     for col in df.columns:
-#         # Check if the cell contains a text value (string)
-#         if isinstance(row[col], str):
-#             print(f"\nOriginal value in ({index}, {col}): {row[col]}")
+input_file_path = '../data/All Kit Fit Male Comments.xlsx'
+output_file_path = '../data/Translated Kit Fit Male Comments.xlsx'
+sheet_name = 'All Male Comments'
+# Load the Excel file into a DataFrame
+df = pandas.read_excel(input_file_path, sheet_name = sheet_name)
 
 primary_language = "en" # used from spaCy docs for languages listed
 target_language = "EN-GB" # used from DeepL docs for languages listed
@@ -30,6 +25,28 @@ translator = deepl.Translator(auth_key)
 # send one translation request uploading the whole excel file
 # then add back the translated version into
 
+def translate_excel_sheet():
+
+    # Iterate over each cell in the DataFrame
+    for index, row in df.iterrows():
+        for col in df.columns:
+            # Check if the cell contains a text value (string)
+            if isinstance(row[col], str):
+                original_text = row[col]
+
+                # translation operation
+                translated_text = translate_to_english(original_text)
+                if translated_text:
+                    df.at[index, col] = translated_text
+                    print(f"Translated Text at ({index}, {col}): \n\tOriginal: {original_text} \n\tTranslated: {translated_text}")
+                else:
+                    print(f"\nNon Translated Original Text at ({index}, {col}): \n\t{original_text}")
+        break # TODO remove after testing 1 row
+
+
+    # Save the modified DataFrame back to an Excel file
+    df.to_excel(output_file_path, sheet_name = sheet_name, index=False)
+
 def translate_to_english(sourceText):
     if not detect_Language(sourceText) == primary_language: # if any other language than person spoken language then make DeepL API translation request
 
@@ -38,4 +55,4 @@ def translate_to_english(sourceText):
         return result.text
 
 if __name__ == "__main__":
-    print(translate_to_english("Je porte ma veste tous les jours et c'est très désagréable."))
+    translate_excel_sheet()
