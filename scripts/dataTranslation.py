d7a7a89ced983ab1698c5e69f77e8b879beff716
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-25 11:13:24 -0400

    integrated majority english detection into the main translation

--- scripts/dataTranslation.py
+++ scripts/dataTranslation.py
@@ -1,13 +1,13 @@
 import pandas
 import json
 import deepl
-from autoDetectLang import detect_Language
+from autoDetectLang import detect_Language, is_majority_english
 
 input_file_path = '../data/All Kit Fit Male Comments.xlsx'
 output_file_path = '../data/Translated Kit Fit Male Comments.xlsx'
 sheet_name = 'All Male Comments'
 # Load the Excel file into a DataFrame
-df = pandas.read_excel(input_file_path, sheet_name = sheet_name)
+df = pandas.read_excel(output_file_path, sheet_name = sheet_name)
 
 primary_language = "en" # used from spaCy docs for languages listed
 target_language = "EN-GB" # used from DeepL docs for languages listed
@@ -27,20 +27,20 @@ def translate_excel_sheet():
             # Check if the cell contains a text value (string)
             if isinstance(row[col], str):
                 original_text = row[col]
-
+                original_text = original_text.lower()
+                
                 # detected = detect_Language(original_text) # DEBUGGING
                 # if not detected == primary_language:
-                #     print(detected)
-                #     print(f"\nNon Translated Original Text at ({index}, {col}): \n\t{original_text}")
+                #     if not is_majority_english(original_text):
+                #         print(detected)
+                #         print(f"\nNon Translated Original Text at ({index}, {col}): \n\t{original_text}")
 
                 # translation operation
                 translated_text = translate_to_english(original_text)
                 if not translated_text == None and not original_text == translated_text:
-                    df.at[index, col] = translated_text
-                    print(f"Translated Text at ({index}, {col}): \n\tOriginal: {original_text} \n\tTranslated: {translated_text}")
-                # else:
-                #     print(f"\nNon Translated Original Text at ({index}, {col}): \n\t{original_text}")
-
+                    if not is_majority_english(original_text):
+                        df.at[index, col] = translated_text
+                        print(f"Translated Text at ({index}, {col}): \n\tOriginal: {original_text} \n\tTranslated: {translated_text}")
 
     # Save the modified DataFrame back to an Excel file
     df.to_excel(output_file_path, sheet_name = sheet_name, index=False)
@@ -48,7 +48,7 @@ def translate_excel_sheet():
 def translate_to_english(sourceText):
     langdetected = detect_Language(sourceText)
 
-    if langdetected == expected_secondary_language: # if any other language than person spoken language then make DeepL API translation request
+    if not langdetected == primary_language: # if any other language than person spoken language then make DeepL API translation request
         print(langdetected)
         result = translator.translate_text(sourceText, target_lang = target_language)
         
